fe24d5e4900117ae0fccf23988c25802ee78bfac
Swanand Pagnis <swanand.pagnis@gmail.com>
2015-08-31 13:38:11 +0530

    Sidekiq job cleanup

--- app/jobs/subscription_mailer_job.rb
+++ app/jobs/subscription_mailer_job.rb
@@ -8,6 +8,6 @@ class SubscriptionMailerJob < ActiveJob::Base
   rescue ActiveRecord::RecordNotFound
     # fail silently
     # This record has been deleted, so no need to retry
-    Rails.logger.error "Trying to send email to deleted record. Discarding."
+    logger.error { "Trying to send email for a deleted record. Discarding." }
   end
 end
--- app/jobs/subscription_request_mailer_job.rb
+++ app/jobs/subscription_request_mailer_job.rb
@@ -4,5 +4,9 @@ class SubscriptionRequestMailerJob < ActiveJob::Base
   def perform(subscription_request_id)
     subscription_request = SubscriptionRequest.find(subscription_request_id)
     SubscriptionRequestMailer.confirmation(subscription_request).deliver
+  rescue ActiveRecord::RecordNotFound
+    # fail silently
+    # This record has been deleted, so no need to retry
+    logger.error { "Trying to send email for a deleted record. Discarding." }
   end
 end
